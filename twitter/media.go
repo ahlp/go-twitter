234f1594d3daa51283944da9ce5f736994a9eed7
ahlp <ahlp>
2018-02-28 09:59:51 -0300

    adjusting url on media request

--- twitter/media.go
+++ twitter/media.go
@@ -1,11 +1,15 @@
 package twitter
 
-import "github.com/dghubble/sling"
+import (
+	"io"
+	"net/http"
+
+	"github.com/dghubble/sling"
+)
 
 // MediaParams
 type MediaParams struct {
-	Media            string `json:"media"`
-	MediaData        []byte
+	ImageBase64      string `json:"media"`
 	AdditionalOwners string `json:"additional_owners"`
 }
 
@@ -18,12 +22,13 @@ type Media struct {
 	Image    MediaInfo `json:"image"`
 }
 
-func newMediaService(sling *sling.Sling) *StatusService {
-	return &StatusService{
-		sling: sling.Path("media/"),
+func newMediaService(sling *sling.Sling) *MediaService {
+	return &MediaService{
+		sling: sling.Base("https://upload.twitter.com/1.1/").Path("media/"),
 	}
 }
 
+// infos about the media returned by the twitter API
 type MediaInfo struct {
 	ImageType string `json:"image_type"`
 	Width     int16  `json:"w"`
@@ -35,20 +40,20 @@ type MediaService struct {
 	sling *sling.Sling
 }
 
+// Send image in body to twitter
 // POST https://upload.twitter.com/1.1/media/upload.json
-func (m MediaService) uploadImage(params MediaParams) {
-
+func (m MediaService) UploadImage(image io.Reader) (*Media, *http.Response, error) {
 	media := new(Media)
 	apiError := new(APIError)
-	path := "upload.json"
-	resp, err := m.sling.New().Post(path).BodyForm(params).Receive(media, apiError)
-	// return tweet, resp, relevantError(err, *apiError)
+	resp, err := m.sling.New().Post("upload.json").Body(image).Receive(media, apiError)
+	return media, resp, relevantError(err, *apiError)
+}
 
-	// if raw data
-	// if media.MediaData {
-
-	//  return
-	// }
-	// else base64
-
+// Send image in base64 to twitter
+// POST https://upload.twitter.com/1.1/media/upload.json
+func (m MediaService) UploadImageBase64(params MediaParams) (*Media, *http.Response, error) {
+	media := new(Media)
+	apiError := new(APIError)
+	resp, err := m.sling.New().Post("upload.json").BodyForm(params).Receive(media, apiError)
+	return media, resp, relevantError(err, *apiError)
 }
